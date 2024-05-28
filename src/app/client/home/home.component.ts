ce180c905cf168ea9856c1baa873b3acf3375cc3
ErmanFHWien <145124456+ErmanFHWien@users.noreply.github.com>
2024-05-28 23:52:18 +0200

    Product overview
    Set up the mock product overview and prepared so far for admin side

--- src/app/client/home/home.component.ts
+++ src/app/client/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { CheckoutComponent } from '../checkout/checkout.component';
+import { ProductListComponent } from '../products/product-details/product-list/product-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CommonModule, CheckoutComponent],
+  imports: [RouterModule, CommonModule, ProductListComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
