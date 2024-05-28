165d513ce3069da1311e1802c98adaa60916176c
ErmanFHWien <145124456+ErmanFHWien@users.noreply.github.com>
2024-05-28 23:22:52 +0200

    Erman
    Updated the Shopping cart and implemented drop down and checkout buttons

--- src/app/client/home/home.component.ts
+++ src/app/client/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from '../checkout/checkout.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, CheckoutComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
